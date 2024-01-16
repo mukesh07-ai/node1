f1043e2f9571cd650e73a1aaee3f2fb038826103
MUKESH KUMAR VISHWAS <mk5297523@gmail.com>
2024-01-16 14:55:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 http = require('node:http')
 server = http.createServer((req, res) =>{
-    res.writeHead(200, {"Content-Type":"text/plain"})
-    res.end('Get Response')
+    data = {Name:'Mukesh', Phone:'7260926717'}
+    res.writeHead(200, {"Content-Type":"application/json"})
+    res.end(JSON.stringify(data))
 })
 
 server.listen(3000, ()=>{console.log('server Up')})
