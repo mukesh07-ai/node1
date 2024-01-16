f9a179eccd6f97d601d5d96f05a39f64590c61b6
MUKESH KUMAR VISHWAS <mk5297523@gmail.com>
2024-01-16 15:25:43 +0530

    commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 http = require('node:http')
+fs = require('node:fs')
 server = http.createServer((req, res) =>{
+    html = fs.readFileSync('./index.html', 'utf-8')
     //data = {Name:'Mukesh', Phone:'7260926717', age:'21'}
     res.writeHead(200, {"Content-Type":"text/html"})
-    res.end('data')
+    res.end(html)
 })
 
 server.listen(3000, ()=>{console.log('server Up')})
