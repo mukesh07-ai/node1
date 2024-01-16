62b769760844d2280c64140032c7df86fc84a8a4
MUKESH KUMAR VISHWAS <mk5297523@gmail.com>
2024-01-16 14:57:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 http = require('node:http')
 server = http.createServer((req, res) =>{
-    data = {Name:'Mukesh', Phone:'7260926717'}
+    data = {Name:'Mukesh', Phone:'7260926717', age:'21'}
     res.writeHead(200, {"Content-Type":"application/json"})
     res.end(JSON.stringify(data))
 })
