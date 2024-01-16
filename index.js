2e5bdf8374b5a2525c04172f5e759e369a3b3e83
MUKESH KUMAR VISHWAS <114977531+mukeshvishwas@users.noreply.github.com>
2024-01-16 12:06:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Read method
+//a = require('node:fs')
+//b = a.readFileSync('./a.txt', 'utf-8')
+//console.log(b)
+
+// //a.readFile('./a.txt', 'utf-8', (error, data) =>{
+//     if(error){console.log(error)}
+//     else{console.log(data)}
+// //})
+// console.log('hellow')
+// console.log('patna')
+
+// console.log('how are you')
+
+//Write methord
+// a = require('node:fs')
+// a.writeFileSync('./m.txt', ' I am Mukesh')
+
+//Append merhord
+fs = require('node:fs')
+fs.writeFile('./k.txt', ' uyfvtftatna', {flag: "a"}, (error) =>{
+        if(error){console.log(error)}
+        else{console.log('print')}
+    })
