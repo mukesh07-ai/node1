bc601ed7533aeeae545e178409b93fcd45cff1cd
MUKESH KUMAR VISHWAS <mk5297523@gmail.com>
2024-01-16 15:11:13 +0530

    3rd

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 http = require('node:http')
 server = http.createServer((req, res) =>{
-    data = {Name:'Mukesh', Phone:'7260926717', age:'21'}
-    res.writeHead(200, {"Content-Type":"application/json"})
-    res.end(JSON.stringify(data))
+    //data = {Name:'Mukesh', Phone:'7260926717', age:'21'}
+    res.writeHead(200, {"Content-Type":"text/html"})
+    res.end('data')
 })
 
 server.listen(3000, ()=>{console.log('server Up')})
