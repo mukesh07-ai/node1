3695bfa94ca36ff6b45de35de2a48785274f3df6
MUKESH KUMAR VISHWAS <mk5297523@gmail.com>
2024-01-16 15:54:31 +0530

    Update process.js

--- process.js
+++ process.js
@@ -1,4 +1,14 @@
-process = require('node:process');
-process.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-  });
+// Node.js program to demonstrate the 
+// process.argv Property 
+
+// Include process module 
+const process = require('process'); 
+
+// Printing process.argv property value 
+var args = process.argv; 
+
+console.log("number of arguments is "+args.length); 
+
+args.forEach((val, index) => { 
+	console.log(`${index}: ${val}`); 
+}); 
